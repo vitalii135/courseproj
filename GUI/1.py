3a2c6acb5475dd49270e703160013f3614e5bf56
vitalii <vitali4ka45@gmail.com>
2023-04-04 14:21:32 +0300

    basic GUI

---
+++ GUI/1.py
@@ -0,0 +1,9 @@
+import PySimpleGUI as sg
+
+label = sg.Text("Type in a to-do")
+input_box=sg.InputText(tooltip="Enter todo")
+add_button = sg.Button("Add")
+
+window = sg.Window('My App', layout=[[label] , [input_box , add_button] ])
+window.read()
+window.close()
