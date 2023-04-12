226deb53e4cec7feecf464d2d44336cdf78399e0
vitalii <vitali4ka45@gmail.com>
2023-04-12 14:56:39 +0300

    basic GUI 1.2 complete and exit buttons

--- GUI/1.py
+++ GUI/1.py
@@ -5,12 +5,17 @@ label = sg.Text("Type in a to-do")
 input_box=sg.InputText(tooltip="Enter todo",key="todo")
 add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos(),key='todos',
-                      enable_events=True, size=[45,100])
+                      enable_events=True, size=[45,10])
 edit_button = sg.Button("Edit")
+complete_button = sg.button("complete")
+exit_button = sg.Button("Exit")
 
 window = sg.Window('My App',
-                   layout=[[label] , [input_box , add_button][list_box,edit_button]],
-                   font=('Helvetica',13))
+                   layout=[[label] ,
+                           [input_box , add_button],
+                           [list_box, edit_button , complete_button]
+                           [exit_button]],
+                   font=('Helvetica',10))
 
 while True:
     event ,values = window.read()
@@ -32,8 +37,18 @@ while True:
             todos = functions.get_todos()
             index = todos.index(todo_to_edit)
             todos[index] = new_todo
-            functions.write_todo(todos)
+            functions.write_todos(todos)
             window['todos'].update(values=todos)
+        case 'complete':
+            todo_to_complete=values['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value="")
+        case 'Exit':
+            break
+
         case 'todos':
             window['todo'].update(value=values['todos'][0])
 
